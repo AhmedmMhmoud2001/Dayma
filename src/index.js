e9eaffcb90c19e91f55a17048f14f2246104e2ae
Ahmed Mahmoud <86148334+AhmedmMhmoud2001@users.noreply.github.com>
2022-08-29 18:27:52 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 
   
 header_home = ()=>{
-    console.log(scrollY)
  let header_home= document.getElementById("header_home");
     if(scrollY<120 ){
         header_home.classList.add("header_home");
@@ -9,9 +8,4 @@ header_home = ()=>{
     else{
         header_home.classList.remove("header_home");
     }
-}
-let i=1;
-main_carousel_button_bottom = () => {
-    let carouselExampleCaptions= document.getElementById("carouselExampleCaptions")
-    console.log(scrollY)
 }
